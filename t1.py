7ec2116cbb383f8f18facc06e9787c098bb6cc5e
Lamar <lamar32994@gmail.com>
2025-09-14 00:41:10 +0300

    test some functions

---
+++ t1.py
@@ -0,0 +1,55 @@
+
+
+def nameN(name,age):
+    
+    print("Hello", name)
+    print("your age is ",age)
+    
+
+
+
+nameN("lamar",19)
+
+def add(a,b):
+    
+    if a>0 and b>0:
+        print (a+b)
+    else:
+        print("invalid input<<please enter positive numbers only>>")
+
+
+add(9,3)
+add(100,200)
+
+
+def sayhello(*names):
+    
+    
+    
+
+    for n in names:
+        print("hello",n)
+
+
+sayhello("lamar","lama","dyala","lina","lolo")
+
+
+def introduce(name, age=0,city="unknown"):
+    
+    print("Hello, my name is",name)
+    print("I am",age,"years old")
+    print("I live in",city)
+    
+    
+    
+
+introduce("Lamar")
+print("*"*50)
+def showskills (**skills):
+    
+    print("My skills are:")
+    for skill, value in skills.items():
+        print(skill + ":", value)
+
+showskills(programming="python",math="algebra",science="physics")
+
